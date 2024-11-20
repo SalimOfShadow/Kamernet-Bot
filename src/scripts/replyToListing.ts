18c359ccab8d86628ec46a964283f98e62003fe4
Salim <152934001+SalimOfShadow@users.noreply.github.com>
2024-11-20 15:18:04 +0100

    fix bug that caused a button to not be clickable

--- src/scripts/replyToListing.ts
+++ src/scripts/replyToListing.ts
@@ -1,7 +1,7 @@
-import { Page } from "puppeteer";
-import { Settings } from "../bot";
-import { randomMouseClickDelay, wait } from "../utils/randomActions";
-import { filterByDescription } from "./filterByDescription";
+import { Page } from 'puppeteer';
+import { Settings } from '../bot';
+import { randomMouseClickDelay, wait } from '../utils/randomActions';
+import { filterByDescription } from './filterByDescription';
 
 // TODO - Handle the case where only one listing is present
 
@@ -11,10 +11,10 @@ export async function replyToListing(page: Page, settings: Settings) {
   if (foundFilteredWord) return;
   // Close the page if a reply has already been sent
   const messageSentBox: string =
-    "#page-content > div.ListingFound_gridContainer__4AReK > div.ListingFound_rightColumn__e5LwV > section > div.Overview_root__4rJz_ > section:nth-child(4) > div";
+    '#page-content > div.ListingFound_gridContainer__4AReK > div.ListingFound_rightColumn__e5LwV > section > div.Overview_root__4rJz_ > section:nth-child(4) > div';
 
   if ((await page.$(messageSentBox)) !== null) {
-    console.log("Already replayed to this listing.");
+    console.log('Already replayed to this listing.');
     await page.close();
     return;
   }
@@ -23,42 +23,49 @@ export async function replyToListing(page: Page, settings: Settings) {
 
   // Handles 404 pages
   const pageNotFoundMessage: string =
-    "#page-content > section > div > p.MuiTypography-root.MuiTypography-body2.mui-style-15ysfrf";
-  const notAvailableMessage: string = "#page-content > section > h1";
+    '#page-content > section > div > p.MuiTypography-root.MuiTypography-body2.mui-style-15ysfrf';
+  const notAvailableMessage: string = '#page-content > section > h1';
 
   const pageNotFound = await page.$$(pageNotFoundMessage);
   const listingNotAvailable = await page.$$(notAvailableMessage);
 
   if (pageNotFound.length > 0 || listingNotAvailable.length > 0) {
-    console.log("This advert is no longer available.");
+    console.log('This advert is no longer available.');
     await page.close();
     return;
   }
 
   // Navigate to the message page
   const contactLandlordButton: string =
-    "#page-content > div.ListingFound_gridContainer__4AReK > div.ListingFound_rightColumn__e5LwV > section > div.Overview_root__4rJz_ > section:nth-child(4) > a";
+    '#page-content > div.ListingFound_gridContainer__4AReK > div.ListingFound_rightColumn__e5LwV > section > div.Overview_root__4rJz_ > section:nth-child(4) > a';
 
   await page.waitForSelector(contactLandlordButton);
   await wait(1000, 2000);
 
-  await page.click(contactLandlordButton, { delay: randomMouseClickDelay() });
+  await page.evaluate((buttonSelector) => {
+    const button = document.querySelector(buttonSelector);
+    if (button && button instanceof HTMLAnchorElement) {
+      // Cast the element to HTMLAnchorElement to access the 'click' method
+      button.click();
+    }
+  }, contactLandlordButton);
+  // await page.click(contactLandlordButton, { delay: randomMouseClickDelay() });
 
   // Fill out the reply field and send a message to the landlord
   await contactLandlord(page, settings);
 }
 
 async function contactLandlord(page: Page, settings: Settings) {
-  const replyField: string = "#Message";
+  const replyField: string = '#Message';
   const sendMessageButton =
-    "body > main > div:nth-child(1) > div.container > div > form > div.barrier-questions__footer > button";
+    'body > main > div:nth-child(1) > div.container > div > form > div.barrier-questions__footer > button';
 
   await wait(2000, 2001); // TODO - FIGURE OUT WHY WE NEED TO WAIT 2 SECONDS TO GUARANTEE THAT THE REPLY FIELD IS LOADED
 
   const messageSelector = await page.$$(replyField);
 
   if (messageSelector.length > 0) {
-    console.log("Found the message selector!");
+    console.log('Found the message selector!');
     await page.click(replyField);
   } else {
     console.log("Couldn't find the message selector,retrying in 2 seconds");
@@ -69,10 +76,10 @@ async function contactLandlord(page: Page, settings: Settings) {
 
   await page.waitForSelector(replyField);
   // TODO - COPY AND PASTE THE TEXT INSTEAD OF TYPING
-  await page.type(replyField, settings.customReplyRoom || "");
+  await page.type(replyField, settings.customReplyRoom || '');
 
-  await page.click(sendMessageButton, { delay: randomMouseClickDelay() });
-
+  console.log('SHOULD CLICK THE BUTTON');
+  // await page.click(sendMessageButton, { delay: randomMouseClickDelay() });
   console.log(`Replied to this listing :  ${page.url()}`);
   await wait(1000, 2000);
 
